a02a9f54aaa525f780707c0e6a6d288d6297ec0d
youlam69 <ylamraou@student.1337.ma>
2023-05-01 02:55:40 +0100

    cpp 01 05

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylamraou <ylamraou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/01 00:09:34 by ylamraou          #+#    #+#             */
+/*   Updated: 2023/05/01 00:09:35 by ylamraou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include <string>
 
@@ -11,8 +23,8 @@ int main(int ac, char **av)
 		while(av[p])
 		{
 			for (size_t i = 0; i < std::strlen(av[p]); i++) {
-				av[1][i] = std::toupper(av[p][i]);
-				std::cout << av[1][i];;
+				av[p][i] = std::toupper(av[p][i]);
+				std::cout << av[p][i];;
 			}
 			if(p + 1 < ac)
 				std::cout << " ";
--- cpp_module_00/ex01/Contact.hpp
+++ cpp_module_00/ex01/Contact.hpp
@@ -6,13 +6,12 @@
 /*   By: ylamraou <ylamraou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 00:07:38 by ylamraou          #+#    #+#             */
-/*   Updated: 2023/05/01 01:21:20 by ylamraou         ###   ########.fr       */
+/*   Updated: 2023/05/01 01:35:56 by ylamraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
-// #include <iomanip>
 
 class Contact
 {
@@ -22,7 +21,6 @@ class Contact
 		std::string nickname;
 		std::string phone_number;
 		std::string darkest_secret;
-
 	public:
 		Contact();
 		void set_infos(std::string first_name, std::string last_name, std::string nickname, std::string phone_number, std::string darkest_secret);
