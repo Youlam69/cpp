d5115a5fdca5930d908049bf007f73990187a6c9
youlam69 <ylamraou@student.1337.ma>
2023-05-01 02:57:38 +0100

    cpp 01 05 v

--- cpp_module_00/ex01/Contact.hpp
+++ cpp_module_00/ex01/Contact.hpp
@@ -6,13 +6,13 @@
 /*   By: ylamraou <ylamraou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 00:07:38 by ylamraou          #+#    #+#             */
-/*   Updated: 2023/05/01 01:35:56 by ylamraou         ###   ########.fr       */
+/*   Updated: 2023/05/01 01:45:53 by ylamraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
-
+#include  <iomanip>
 class Contact
 {
 	private:
--- cpp_module_00/ex01/main.cpp
+++ cpp_module_00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ylamraou <ylamraou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 00:07:35 by ylamraou          #+#    #+#             */
-/*   Updated: 2023/05/01 01:29:01 by ylamraou         ###   ########.fr       */
+/*   Updated: 2023/05/01 02:29:23 by ylamraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,64 @@ bool isNumber(const std::string& str)
 {
     return str.find_first_not_of("0123456789") == std::string::npos;
 }
+bool add_contact(PhoneBook& phonebook)
+{
+			Contact_add contact;
+			std::cout << "Enter First Name" << std::endl;
+		if	(!std::getline(std::cin , contact.first_name).good())
+		{
+			exit(1);
+		}				// if(std::cin.eof())
+			// 	return 1;
+			std::cout << "Enter Last Name" << std::endl;
+			// std::cin >> contact.last_name;
+			// if(std::cin.eof())
+			// 	return 1;
+		if(!std::getline(std::cin , contact.last_name).good())
+		{
+			exit(1);
+		}			// std::cin >> contact.darkest_secret
+			std::cout << "Enter Nickname" << std::endl;
+			
+			// std::cin >> contact.nickname;
+			// if(std::cin.eof())
+			// 	return 1;
+		if(!std::getline(std::cin , contact.nickname).good())
+		{
+			exit(1);
+		}
+			std::cout << "Enter Phone Number" << std::endl;
+			while(1)
+			{
+				if(!std::getline(std::cin , contact.phone_number).good())
+				{
+					exit(1);
+				}				
+				// std::cin >> contact.phone_number;
+				// if(std::cin.eof())
+				// 	return 1;
+				if(contact.phone_number.length() > 10 || !isNumber(contact.phone_number))
+				{
+					std::cout << "-1- Don't use anything but numbers;" << std::endl;
+					std::cout << "-2- Don't use more than 10 digits." << std::endl;
+					std::cout << "-3- Remember " "-1-  -2-" " & try again " << std::endl;
+				}
+				else
+					break;
+			}
+			std::cout << "Enter Darkest Secret" << std::endl;
+			// std::cin.clear();
+		if(!std::getline(std::cin , contact.darkest_secret).good())
+		{
+			exit(1);
+		}
+			// std::cin >> contact.darkest_secret;
+			if (contact.first_name.empty() || contact.last_name.empty() || contact.nickname.empty() || contact.phone_number.empty() || contact.darkest_secret.empty())
+				std::cout << "Concat can't have empty fields." << std::endl;
+			else
+				phonebook.add(contact.first_name, contact.last_name, contact.nickname, contact.phone_number, contact.darkest_secret);
+	return 0;
+}
 
 int main() {
 	
@@ -38,40 +96,51 @@ int main() {
 			break;
 		std::string command;
 		std::cout << "Enter command: " << std::endl;
-		std::cin >> command;
+		// std::cin >> command;
+		if(!std::getline(std::cin ,command).good())
+		{
+			exit(1);
+		}
+
 		// getline(std::cin, test);
 		// std::cout << test << std::endl;
 		// std::cout << command << std::endl;
 		if (command == "EXIT")
 			break;
+		// else if (command.compare("ADD") != std::string::npos)
 		else if (command == "ADD")
 		{
-			Contact_add contact;
-			std::cout << "Enter First Name" << std::endl;
-			std::cin >> contact.first_name;
-			std::cout << "Enter Last Name" << std::endl;
-			std::cin >> contact.last_name;
-			std::cout << "Enter Nickname" << std::endl;
-			std::cin >> contact.nickname;
-			std::cout << "Enter Phone Number" << std::endl;
-			while(1)
-			{
-				std::cin >> contact.phone_number;
-				if(std::cin.eof())
-					return 1;
-				if(contact.phone_number.length() > 10 || !isNumber(contact.phone_number))
-				{
-					std::cout << "-1- Don't use anything but numbers;" << std::endl;
-					std::cout << "-2- Don't use more than 10 digits." << std::endl;
-					std::cout << "-3- Remember " "-1-  -2-" " & try again " << std::endl;
-				}
-				else
-					break;
-			}
-			std::cout << "Enter Darkest Secret" << std::endl;
-			std::cin.clear();
-			std::cin >> contact.darkest_secret;
-			phonebook.add(contact.first_name, contact.last_name, contact.nickname, contact.phone_number, contact.darkest_secret);
+			std::cout << command << std::endl;
+			
+			if(add_contact(phonebook))
+				break;
+			
+			// Contact_add contact;
+			// std::cout << "Enter First Name" << std::endl;
+			// std::cin >> contact.first_name;
+			// std::cout << "Enter Last Name" << std::endl;
+			// std::cin >> contact.last_name;
+			// std::cout << "Enter Nickname" << std::endl;
+			// std::cin >> contact.nickname;
+			// std::cout << "Enter Phone Number" << std::endl;
+			// while(1)
+			// {
+			// 	std::cin >> contact.phone_number;
+			// 	if(std::cin.eof())
+			// 		return 1;
+			// 	if(contact.phone_number.length() > 10 || !isNumber(contact.phone_number))
+			// 	{
+			// 		std::cout << "-1- Don't use anything but numbers;" << std::endl;
+			// 		std::cout << "-2- Don't use more than 10 digits." << std::endl;
+			// 		std::cout << "-3- Remember " "-1-  -2-" " & try again " << std::endl;
+			// 	}
+			// 	else
+			// 		break;
+			// }
+			// std::cout << "Enter Darkest Secret" << std::endl;
+			// std::cin.clear();
+			// std::cin >> contact.darkest_secret;
+			// phonebook.add(contact.first_name, contact.last_name, contact.nickname, contact.phone_number, contact.darkest_secret);
 		}
 		else if (command == "SEARCH")
 		{
@@ -83,16 +152,23 @@ int main() {
 			std::cout << "Enter index: " << std::endl;
 			std::string index;
 			// int index;
-			std::cin >> index;
+			// std::cin >> index;
+				if(!std::getline(std::cin ,index).good())
+					{
+						exit(1);
+					}
 			// while (!isNumber(index)){
 			// 	if(std::cin.eof())
 			// 		break;
 			// 	std::cout << "Enter index: " << std::endl;
 			// 	std::cin >> index;
 			// }
-			phonebook.search(std::atoi(index.c_str()));
-			// phonebook.search(index);
-				
+
+			// length 1 and char is num
+			if (index.length() == 1)
+				phonebook.search(std::atoi(index.c_str()));
+			else
+				std::cout << "Invalid ID" << std::endl; 
 			}
 		}
 		else
