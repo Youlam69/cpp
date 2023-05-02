23ee02e18ae374b947407f2daf96f004ccaa045b
youlam69 <ylamraou@student.1337.ma>
2023-05-02 05:12:27 +0100

    cpp ex02

---
+++ cpp_module_01/ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylamraou <ylamraou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/02 04:56:35 by ylamraou          #+#    #+#             */
+/*   Updated: 2023/05/02 05:11:56 by ylamraou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+	std::string stringA = "HI THIS IS BRAIN";
+	std::string *stringPTR = &stringA;
+	std::string &stringREF = stringA;
+
+	std::cout << &stringA << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << stringA << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+
+	stringREF.append("opp");
+	std::cout << stringA << std::endl;
+}
