366e12063c24765426d9d69dd3668a91bcaa5929
youlam69 <youlam1337@gmail.com>
2023-09-06 11:43:09 +0100

    07

--- cpp_module_07/ex00/main.cpp
+++ cpp_module_07/ex00/main.cpp
@@ -1,7 +1,24 @@
 #include "whatever.hpp"
 #include <iostream>
 
-int main()
+
+int main( void ) {
+int a = 2;
+int b = 3;
+swap( a, b );
+std::cout << "a = " << a << ", b = " << b << std::endl;
+std::cout << "min( a, b ) = " << min( a, b ) << std::endl;
+std::cout << "max( a, b ) = " << max( a, b ) << std::endl;
+std::string c = "chaine1";
+std::string d = "chaine2";
+swap(c, d);
+std::cout << "c = " << c << ", d = " << d << std::endl;
+std::cout << "min( c, d ) = " << min( c, d ) << std::endl;
+std::cout << "max( c, d ) = " << max( c, d ) << std::endl;
+return 0;
+}
+
+/* int main()
 {
     int a = 2;
     int b = 3;
@@ -37,4 +54,4 @@ int main()
     std::cout << "g: " << g << ", h: " << h << std::endl;
     std::cout << "min(g, h): " << min(g, h) << std::endl;
     std::cout << "max(g, h): " << max(g, h) << std::endl;
-}
+} */
--- cpp_module_07/ex00/whatever.hpp
+++ cpp_module_07/ex00/whatever.hpp
@@ -2,22 +2,23 @@
 
 #include <iostream>
 
-template <typename T>
-void swap(T &a, T &b)
-{
-    T tmp = a;
-    a = b;
-    b = tmp;
-}
+    template <typename T>
+    void swap(T &x, T &y)
+    {
+        T tmp = x;
+        x = y;
+        y = tmp;
+    }
 
-template <typename T>
-T min(T &a, T &b)
-{
-    return (a < b ? a : b);
-}
+    template <typename T>
+    T min(T &x, T &y)
+    {
+        return (x < y ? x : y);
+    }
 
-template <typename T>
-T max(T &a, T &b)
-{
-    return (a > b ? a : b);
-}
+    template <typename T>
+    T max(T &x, T &y)
+    {
+        return (x > y ? x : y);
+    }
+
