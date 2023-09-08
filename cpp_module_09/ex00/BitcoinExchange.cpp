65f79dd4610abfa0dab0f9f2761ff942efece809
Youness Lamraoui <ylamraou@e3r11p8.1337.ma>
2023-09-08 06:20:24 +0100

    09

--- cpp_module_09/ex00/BitcoinExchange.cpp
+++ cpp_module_09/ex00/BitcoinExchange.cpp
@@ -1,12 +1,109 @@
 #include "BitcoinExchange.hpp"
 
+
+BitcoinExchange::BitcoinExchange(){}
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &other)
+{
+    *this = other;
+}
+BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &other)
+{
+    if(this != &other)
+    {
+        this->_map = other._map;
+    }
+    return *this;
+}
+BitcoinExchange::~BitcoinExchange(){}
+
+BitcoinExchange::BitcoinExchange(std::string &data_content, std::string &input_content)
+{
+    while (data_content.find(',') != std::string::npos)
+    {
+        std::string _date = data_content.substr(0, data_content.find(','));
+        data_content.erase(0, data_content.find(',') + 1);
+        float _value = std::atof(data_content.substr(0, data_content.find('\n')).c_str());
+        data_content.erase(0, data_content.find('\n') + 1);
+        _map.insert(std::make_pair(_date, _value));
+    }
+    valid_input(input_content);
+}
+
+
+float BitcoinExchange::valid_pipe(std::string &line)
+{
+    std::string date = line.substr(0, 10);
+    line.erase(0, 10);
+    if(line.find('|') == std::string::npos)
+        throw std::runtime_error("Error: bad input => " + date);
+    for(size_t i = 0 ; i < line.length() && line[i] != '|'; i++)
+    {
+        if(line[i] != ' ')   
+            throw std::runtime_error("Error: bad input => " + date);
+    }
+    line.erase(0, line.find('|') + 1);
+    while(line[0] == ' ')
+        line.erase(0, 1);
+    if(line.length() == 0)
+        throw std::runtime_error("Error: bad input => " + date);
+    int ref = 0;
+    for(size_t i = 0; i < line.length()  ;i++)
+    {
+        if(i == 0 && line[i] == '-')
+            throw std::runtime_error("Error: not a positive number.");
+        if(line[i] == '.')
+            ref++;
+        if(ref > 1)
+            throw std::runtime_error("Error: bad input => " + date);
+        if((line[i] < '0' || line[i] > '9') && line[i] != '.')
+            throw std::runtime_error("Error: bad input => " + date);
+    }
+    float value = std::atof(line.c_str());
+    if( value > 1000 )
+        throw std::runtime_error("Error: too large a number." + date);
+    return value;
+}
+
+void BitcoinExchange::valid_input(std::string &input_content)
+{
+    std::map<std::string, float>::iterator it;
+    std::map<std::string, float>::iterator it1;
+    while(input_content.length() > 0)
+    {
+        std::string line ;
+        if(input_content.find('\n') != std::string::npos)
+            line = input_content.substr(0, input_content.find('\n'));
+        else
+            line = input_content.substr(0, input_content.length());
+        try{
+            valid_date(line);
+            std::string date = line.substr(0, 10);
+            float _value = valid_pipe(line);
+            it = _map.find(date);
+            if(it != _map.end())
+                std::cout <<  date << " => " <<  _value << " = " << it->second * _value << std::endl;
+            else
+                { it1 = _map.lower_bound(date);
+                it1--;
+                std::cout <<  date << " => " <<  _value << " = " << it1->second * _value << std::endl;}
+        }
+        catch(const std::exception& e)
+        {
+            std::cout << e.what() << '\n';
+        }
+        if(input_content.find('\n') != std::string::npos)
+            input_content.erase(0, input_content.find('\n') + 1);
+        else
+            input_content.erase(0, input_content.length());
+    }
+}
+
 void valid_value(std::string value)
 {
     if(value.length() < 2)
-        throw std::runtime_error("Wrong data format1");
+        throw std::runtime_error("Wrong data format");
     if(value[0] != ',')
-        throw std::runtime_error("Wrong data format2");
-    
+        throw std::runtime_error("Wrong data format");
     std::string ref_value = value.substr(1, value.length() - 1);
     int ref = 0;
     for(size_t i = 0; i < ref_value.length()  ;i++)
@@ -15,9 +112,9 @@ void valid_value(std::string value)
         if(ref_value[i] == '.')
             ref++;
         if(ref > 1)
-            throw std::runtime_error("Wrong data format3");
+            throw std::runtime_error("Wrong data format");
         if((ref_value[i] < '0' || ref_value[i] > '9') && ref_value[i] != '.')
-            throw std::runtime_error("Wrong data format4");
+            throw std::runtime_error("Wrong data format");
     }
 }
 
@@ -67,8 +164,6 @@ void valid_data(std::string data)
     if(data.length() <= 10)
         throw std::runtime_error("Wrong data format");
     valid_date(data.substr(0, 10));
-    // std::cout << data + " yolam " << data.size() << std::endl;
-    // std::cout << data.substr(10, data.length() - 10)  << std::endl;
     valid_value(data.substr(10, data.length() - 10));
 }
 
--- cpp_module_09/ex00/BitcoinExchange.hpp
+++ cpp_module_09/ex00/BitcoinExchange.hpp
@@ -20,91 +20,14 @@ class BitcoinExchange
         std::map<std::string, float > _map;
 
     public:
-        float valid_pipe(std::string &line)
-        {
-            std::string date = line.substr(0, 10);
-            line.erase(0, 10);
-            if(line.find('|') == std::string::npos)
-                throw std::runtime_error("Error: bad input => " + date);
-            for(int i = 0 ; i < line.length() && line[i] != '|'; i++)
-            {
-                if(line[i] != ' ')   
-                    throw std::runtime_error("Error: bad input => " + date);
-            }
-            line.erase(0, line.find('|') + 1);
-            while(line[0] == ' ')
-                line.erase(0, 1);
-            if(line.length() == 0)
-                throw std::runtime_error("Error: bad input => " + date);
-            int ref = 0;
-            for(size_t i = 0; i < line.length()  ;i++)
-            {
-                if(i == 0 && line[i] == '-')
-                    throw std::runtime_error("Error: not a positive number.");
-                if(line[i] == '.')
-                    ref++;
-                if(ref > 1)
-                    throw std::runtime_error("Error: bad input => " + date);
-                if((line[i] < '0' || line[i] > '9') && line[i] != '.')
-                    throw std::runtime_error("Error: bad input => " + date);
-            }
-            float value = std::atof(line.c_str());
-            if( value > 1000 )
-                throw std::runtime_error("Error: too large a number." + date);
-            return value;
-        }
 
-        void valid_input(std::string &input_content)
-        {
-            std::map<std::string, float>::iterator it;
-            std::map<std::string, float>::iterator it1;
-            while(input_content.length() > 0)
-            {
-                std::string line ;
-                if(input_content.find('\n') != std::string::npos)
-                    line = input_content.substr(0, input_content.find('\n'));
-                else
-                    line = input_content.substr(0, input_content.length());
-                try{
-                    valid_date(line);
-                    std::string date = line.substr(0, 10);
-                    float _value = valid_pipe(line);
-                    it = _map.find(date);
-                    if(it != _map.end())
-                        std::cout <<  date << " => " <<  _value << " = " << it->second * _value << std::endl;
-                    else
-                      { it1 = _map.lower_bound(date);
-                        it1--;
-                        std::cout <<  date << " => " <<  _value << " = " << it1->second * _value << std::endl;}
-                }
-                catch(const std::exception& e)
-                {
-                    std::cout << e.what() << '\n';
-                }
-                if(input_content.find('\n') != std::string::npos)
-                   input_content.erase(0, input_content.find('\n') + 1);
-                else
-                    input_content.erase(0, input_content.length());
-            }
-        }
-
-        BitcoinExchange(std::string &data_content, std::string &input_content)
-        {
-            while (data_content.find(',') != std::string::npos)
-            {
-                std::string _date = data_content.substr(0, data_content.find(','));
-                data_content.erase(0, data_content.find(',') + 1);
-                float _value = std::atof(data_content.substr(0, data_content.find('\n')).c_str());
-                data_content.erase(0, data_content.find('\n') + 1);
-                _map.insert(std::make_pair(_date, _value));
-            }
-            valid_input(input_content);
-        }
         BitcoinExchange();
-
-        ~BitcoinExchange(){}
-        
-        
+        ~BitcoinExchange();
+        BitcoinExchange(std::string &data_content, std::string &input_content);
+        BitcoinExchange(const BitcoinExchange &other);
+        BitcoinExchange &operator=(const BitcoinExchange &other);
+        void valid_input(std::string &input_content);
+        float valid_pipe(std::string &line);
 
 };
 
