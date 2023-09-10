43d20f051b7dfb3c06c6b1f2f6ef0b31a424eaa7
Youness Lamraoui <ylamraou@e3r11p4.1337.ma>
2023-09-10 07:17:27 +0100

    09

--- cpp_module_09/ex01/RPN.hpp
+++ cpp_module_09/ex01/RPN.hpp
@@ -10,15 +10,12 @@ class RPN
         std::stack<int> _stack;
 
     public:
-    RPN();
-    RPN(const RPN &other);
-    RPN &operator=(const RPN &other);
-    ~RPN();
-    void push_to_stack(int value);
-    void pop_stack();
-    int top_stack();
-    size_t stack_size();
-
-
-
+        RPN();
+        RPN(const RPN &other);
+        RPN &operator=(const RPN &other);
+        ~RPN();
+        void push_to_stack(int value);
+        void pop_stack();
+        int top_stack();
+        size_t stack_size();
 };
